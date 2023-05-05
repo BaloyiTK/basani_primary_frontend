17f674fac26df49a47f6adc712ea7a71211cc623
Tiyani Baloyi <tiyanikevinbaloyi@gmail.com>
2023-05-05 16:48:12 +0200

    updates

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -4,7 +4,7 @@ const AboutCard = ({ imgSrc, title, description }) => {
   return (
     <div className="flex flex-col justify-center items-center md:w-[85%] p-2 mx-auto rounded-lg shadow-lg mt-8 md:mt-0 text-white">
       <img
-        className="h-64 w-64 object-cover"
+        className="md:h-64 md:w-64 object-cover"
         src={imgSrc}
         alt={title}
       />
