fe75977bb9bbf5ca7192f9a2fd312a4e429f44e6
Tiyani Baloyi <tiyanikevinbaloyi@gmail.com>
2023-05-08 11:57:28 +0200

    addding school history

--- src/components/uniform/Uniform.jsx
+++ src/components/uniform/Uniform.jsx
@@ -1,44 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import UniformCard from './UniformCard';
-import api_endpoint from '../../utils/config';
-import axios from 'axios';
-import Spinner from '../Spinner';
+import React, { useState, useEffect } from "react";
+import UniformCard from "./UniformCard";
+import api_endpoint from "../../utils/config";
+import axios from "axios";
+import Spinner from "../Spinner";
 
 const Uniform = () => {
   const [uniforms, setUniforms] = useState([]);
-  const shopAddress = 'xxx xxxx Street, Chiawelo Soweto 1818';
-  const [loading, setloading] = useState(true)
+  const shopAddress = "606 Mandiwana Street, Chiawelo ext 1, Soweto 1818";
+  const tradingHours = "Monday to Fiday: 07:00 - 18:00";
+  const contactNumber = "079 449 4917 / 071 907 1975";
+  const [loading, setloading] = useState(true);
 
   useEffect(() => {
     const fetchUniforms = async () => {
       const response = await axios.get(`${api_endpoint}/api/uniform`);
       setUniforms(response.data.uniform);
-      setloading(false)
+      setloading(false);
     };
 
     fetchUniforms();
   }, []);
 
-
-
   return (
     <div className="container mx-auto mt-20">
-
-      {loading ? <div><Spinner/></div>: <div> <h1 className="text-3xl font-bold mb-8">Official Uniform</h1>
-      <p className="mb-4">Shop Address :{shopAddress}</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 shadow-md">
-        {uniforms &&
-          uniforms.map((uniform) => (
-            <div key={uniform._id} className="relative">
-              <UniformCard uniform={uniform} />
-  
-            </div>
-          ))}
-      </div></div>}
-     
+      {loading ? (
+        <div>
+          <Spinner />
+        </div>
+      ) : (
+        <div>
+          {" "}
+          <h1 className="text-3xl font-bold mb-8">Official Uniform</h1>
+          <p className="font-semibold">
+            Shop Address :{<span className="font-normal">{shopAddress}</span>}
+          </p>
+          <p className=" font-semibold">
+            Trading Hours :{<span className="font-normal">{tradingHours}</span>}
+          </p>
+          <p className="mb-4 font-semibold">
+            Contacts Number :
+            {<span className="font-normal">{contactNumber}</span>}
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 shadow-md">
+            {uniforms &&
+              uniforms.map((uniform) => (
+                <div key={uniform._id} className="relative">
+                  <UniformCard uniform={uniform} />
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Uniform;
-
