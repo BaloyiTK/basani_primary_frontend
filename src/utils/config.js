49f3320c3fadea0952a98208869ee496255b88aa
Tiyani Baloyi <tiyanikevinbaloyi@gmail.com>
2023-05-21 11:01:23 +0200

    updates

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,4 @@
 const api_endpoint = "https://basani-primary-backend.onrender.com";
-<<<<<<< HEAD
 //const api_endpoint = 'http://localhost:3001'
-=======
-//const api_endpoint = 'http://localhost:3001';
->>>>>>> 4cf452003c4b0c45d263c7e30e46f1be55f3d226
 
 export default api_endpoint;
